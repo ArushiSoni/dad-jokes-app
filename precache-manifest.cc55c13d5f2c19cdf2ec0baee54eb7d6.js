c40a17b0db347da0590984760981f59ac3882dbb
Arushi Soni <arushisoni01@gmail.com>
2019-10-02 09:27:05 +0530

    Updates

---
+++ precache-manifest.cc55c13d5f2c19cdf2ec0baee54eb7d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081dd4367134b5fbd98c71a2dcfeb6d2",
+    "url": "/dad-jokes-app/index.html"
+  },
+  {
+    "revision": "4da08e01850ac5bba762",
+    "url": "/dad-jokes-app/static/css/main.553d932d.chunk.css"
+  },
+  {
+    "revision": "5df878ce75f40c3490a1",
+    "url": "/dad-jokes-app/static/js/2.e3e8a200.chunk.js"
+  },
+  {
+    "revision": "4da08e01850ac5bba762",
+    "url": "/dad-jokes-app/static/js/main.dcc40f2b.chunk.js"
+  },
+  {
+    "revision": "bb7c8dc3418dd50d17f3",
+    "url": "/dad-jokes-app/static/js/runtime-main.25d9743b.js"
+  }
+]);
